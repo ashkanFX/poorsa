b64deb93612ba8e8e092109037a8e4fb4864da28
ashkanFX <afotohi@ai-copilot.dev>
2025-07-12 22:52:54 +0330

    change

--- src/app/routes/layout/product/product.component.ts
+++ src/app/routes/layout/product/product.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
-import {TableModule} from "primeng/table";
+import {Component} from '@angular/core';
 import {CardModule} from "primeng/card";
 import {AvatarModule} from "primeng/avatar";
+import {TableModule} from "primeng/table";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CardModule , AvatarModule ],
+  imports: [CardModule, AvatarModule, TableModule, NgClass],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
-
+  products    = [
+    { name: 'Urine bottle', sterility: 'ST', color: 'Clear / Orange' },
+    { name: 'Urine bottle', sterility: 'UN_ST', color: 'Clear / Green' }
+  ];
+  products2 = [
+    { sterility: 'ST', color: 'Clear / Orange', caseQty: 500 },
+    { sterility: 'UN_ST', color: 'Clear / Green', caseQty: 700 }
+  ];  products3 = [
+    { sterility: 'ST', color: 'Clear / Orange', caseQty: 500 },
+    { sterility: 'UN_ST', color: 'Clear / Green', caseQty: 700 }
+  ];
 }
